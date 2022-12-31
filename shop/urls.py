fb9574da761b4325e9dc59a155ebec24e6b07de3
vcyh19 <cvincentc@hotmail.com>
2022-12-31 00:21:47 -0800

    product page

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('products/', views.products, name='products')
 ]
--- shop/views.py
+++ shop/views.py
@@ -10,3 +10,11 @@ def index(request):
     context['products'] = products
     context['rate'] = 24
     return render(request, 'shop/home/index.html', context)
+
+def products(request):
+    context = {}
+    alerts = []
+    products = Product.objects.all()
+    context['products'] = products
+    context['rate'] = 24
+    return render(request, 'shop/home/products.html', context)
