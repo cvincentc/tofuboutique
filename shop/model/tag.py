cec4dd95c8cb4f34260b1852ced2475eda2f76c1
vcyh19 <cvincentc@hotmail.com>
2023-01-30 14:51:47 -0800

    minor change

--- shop/model/tag.py
+++ shop/model/tag.py
@@ -6,7 +6,7 @@ class Tag(models.Model):
     product = models.ForeignKey(to="shop.Product", related_name='related_tags', null=False, blank=False, on_delete=models.PROTECT)
     sku = models.CharField(max_length=50, blank=True, null=True)
     barcode = models.CharField(max_length=50, blank=True, null=True)
-    color = models.ForeignKey(to="shop.Color", related_name='related_tags', on_delete=models.PROTECT, blank=True, null=False)
+    color = models.ForeignKey(to="shop.Color", related_name='related_tags', on_delete=models.PROTECT, blank=True, null=True)
     size_list = models.ManyToManyField(to="shop.Size", blank=True)
     creation_date = models.DateTimeField(null=True, blank=True, default=datetime.now, editable=False)
     token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
